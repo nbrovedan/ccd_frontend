58f747fc34686984a5cad9526250d643cc72776d
BROGNOLI\naidion.brovedan <brovedan@gmail.com>
2019-06-12 10:11:37 -0300

    Alterações

--- app/scripts/services/config.js
+++ app/scripts/services/config.js
@@ -11,13 +11,6 @@ angular.module('frontApp')
   .service('Config', function () {
     return {
         serverUrl: 'http://localhost:8888',
-        //serverUrl: 'http://127.0.0.1:8800',
-        //serverUrl: 'http://10.17.1.169:8181',
-        //serverUrl: 'http://192.168.1.104:8181',
-        //serverUrl: 'http://172.16.1.93:8181',
-        //serverUrl: 'http://192.168.25.89:8181',
-        //serverUrl: 'http://192.168.1.27:8181',
-        //serverUrl: 'http://172.16.200.149:8181',
         pass: '9JAFMBrfLS6qCm&c6CgpA0Wq1&U3MsK#VxGr4p8U@Rr#mqjx=='
     };
   });
